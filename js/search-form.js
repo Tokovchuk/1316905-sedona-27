9027d676c06b5a92f46701190a9b6efbfe3eacda
Tokovchuk <i.tokovchuk@gmail.com>
2020-03-18 17:52:46 +0300

    Удалил js

--- js/search-form.js
+++
@@ -1,7 +0,0 @@
-var open = document.querySelector(".button-title-search");
-var form = document.querySelector(".form-without-title");
-
-open.addEventListener("click", function(evt){	
-evt.preventDefault();
-form.classList.toggle("form-hide");
-})
