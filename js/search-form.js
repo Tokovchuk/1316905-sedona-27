6e9fd0b773be208bb04bfb25e63f624d20bd4356
Tokovchuk <i.tokovchuk@gmail.com>
2020-03-21 13:00:38 +0300

    Добавил анимацию формы

--- js/search-form.js
+++ js/search-form.js
@@ -1,7 +1,27 @@
 var open = document.querySelector(".button-title-search");
-var form = document.querySelector(".form-without-title");
+var form = document.querySelector(".form-s");
+var ff = document.querySelector(".form-without-title")
+var checkin = form.querySelector("[name=check-in-date]")
+var checkout = form.querySelector("[name=check-out-date]")
+var adults = form.querySelector("[name=number-of-adults]")
+var children = form.querySelector("[name=number-of-children]")
 
 open.addEventListener("click", function(evt){	
-evt.preventDefault();
-form.classList.toggle("form-hide");
-})
+	evt.preventDefault();
+	ff.classList.remove("error-form");
+	open.classList.remove("error-form");
+	ff.classList.toggle("close-form");
+});
+
+form.addEventListener("submit", function(evt){	
+	if (!checkin.value || !checkout.value || !adults.value || !children.value) {
+		evt.preventDefault();
+		ff.classList.remove("error-form");
+     	ff.offsetWidth = ff.offsetWidth;
+     	open.classList.remove("error-form");
+     	open.offsetWidth = ff.offsetWidth;
+      	ff.classList.add("error-form");
+      	open.classList.add("error-form");
+	}
+});
+
